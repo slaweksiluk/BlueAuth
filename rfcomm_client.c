7556c2b2636452dd82cb37adf4822663b4e1ce46
Slawomir Siluk <slaweksiluk@gazeta.pl>
2014-01-22 09:54:47 +0100

    jednokrotne polaczenie

--- rfcomm_client.c
+++ rfcomm_client.c
@@ -12,33 +12,43 @@ int main(int argc, char **argv)
 	struct sockaddr_rc addr = { 0 };
 	int s, status;
 	char buf[6] = {0};
+	char compare = "u";
 	char dest[18] = "8c:71:f8:99:f0:73";
-	// allocate a socket
+	printf("allocate a socket\n");
 	s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 	// set the connection parameters (who to connect to)
 	addr.rc_family = AF_BLUETOOTH;
 	addr.rc_channel = (uint8_t) 9;
 	str2ba( dest, &addr.rc_bdaddr );
-	// connect to server
+	printf("connect to server\n");
 	status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
 	// send a message
 	if( status == 0 ) {
 		status = write(s, "hello!", 6);
 		if( status < 0 ) {
-			perror("write error");
+			perror("write error\n");
 			close(s);
-			return 0;
+			return -1;
 		}
 		status = read(s, buf, sizeof(buf));
-		printf("Recived: %s", buf);
-	}	
-	if( status < 0 ) perror("read error");
+		printf("Recived: %s\n", buf);
 		close(s);
-	if(buf[0] == "u"){
-		printf("return: 0");
-		return 0;
+		if( status < 0 ){
+			perror("read error\n");
+			close(s);
+			return -1;
+		}
+		if(buf[0] == compare){
+			printf("return: 0\n");
+			return 0;
+		}
+	}	
+	else{
+		perror("connect error\n");
+		return -1;
 	}
-	else return -1;
+	printf("return: 0\n");
+	return 0;
 }
 
