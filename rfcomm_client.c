2117db094870b3c751c64ba0c32764b268377a8a
Slawomir Siluk <slaweksiluk@gazeta.pl>
2014-07-17 18:33:46 +0200

    działa zapisyanie i odczytiwanie pliku /etc/blueAuth.conf

--- rfcomm_client.c
+++ rfcomm_client.c
@@ -5,28 +5,56 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
-#include "simplescan.c"
+
 
 
 /*int main(int argc, char **argv)*/
 int main()
 {
-	int status;
-	char dest[18];
-	status = simplescan(dest);
-	if(status != 0) fprintf(stderr, "Unable to find phone with generated .jar installed and runned\n");
-	fprintf(stderr,"Btaddr: %s\n", dest);
+
+  char *dest = NULL;
+  char *port = NULL;
+  char *line = NULL;
+  size_t len = 0;
+
+  ssize_t read_s;
+
+  FILE *fp;
+   fp = fopen("/etc/blueAuth.conf", "r");
+   if (fp == NULL) printf("file not open\n");
+
+	read_s = getline(&line, &len, fp); //komentarz
+	read_s = getline(&line, &len, fp); //UUID
+	read_s = getline(&dest, &len, fp); //btaddr
+
+int dest_len;					//removes newline character
+dest_len = strlen(dest);
+if( dest[dest_len-1] == '\n' )
+    dest[dest_len-1] = 0;
+				printf("%s\n", dest);
+
+	read_s = getline(&port, &len, fp); //port
+int port_int = atoi( port );
+               printf("%d", port_int);
+
+
+//	fprintf(stderr,"Btaddr: %s\n", dest);
+	//fprintf(stderr,"Btaddr: %s\n", &dest);
+
+	
 
 	struct sockaddr_rc addr = { 0 };
+	int status;
 	int s;
 	char buf[6] = {0};
-	char compare = "u";
+	char *compare;
+	compare = "u";
 	//char dest[18] = "8c:71:f8:99:f0:73";
 	printf("allocate a socket\n");
 	s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 	// set the connection parameters (who to connect to)
 	addr.rc_family = AF_BLUETOOTH;
-	addr.rc_channel = (uint8_t) 9;
+	addr.rc_channel = (uint8_t) port_int;
 	str2ba( dest, &addr.rc_bdaddr );
 	printf("connect to server\n");
 	status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
@@ -47,7 +75,7 @@ int main()
 			close(s);
 			return -1;
 		}
-		if(buf[0] == compare){
+		if(buf[0] == *compare){
 			printf("return: 0\n");
 			return 0;
 		}
